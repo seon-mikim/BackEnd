096a4c34fd8bacb4a5fb22ec335314b5b51b7314
seon-mikim <tjsalsun@gmail.com>
2023-09-18 02:06:55 +0900

    feat: 로그인 로그아웃 회원가입 구현

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -12,34 +12,39 @@ dotenv.config();
 
 
 export class UserController {
+  static userRepo;
+  static initialize() {
+    this.userRepo = myDataBase.getRepository(User);
+  }
   static register = async (req: Request, res: Response) => {
-    const { id, userId, password, userName } = req.body;
-
+    console.log(req.body)
+    const { id, userId, password, userName, phoneNumber, userEmail } = req.body;
+   console.log(password)
     const userIdRegx = /^[a-zA-Z0-9]{4,16}$/.test(userId);
     if (!userIdRegx) {
       return res.status(400).json({
         message: 'ID는 숫자와 영문 대소문자 4~16자까지만 가능합니다.',
       });
     }
-
     const userNameRegex = /^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣._-]{1,20}$/;
     if (!userNameRegex.test(userName)) {
       return res.status(400).json({ message: '유효한 닉네임이 아닙니다.' });
     }
-
-    const existUser = await myDataBase.getRepository(User).findOne({
-      where: [{ userId }, { userName }],
+    const existUser = await this.userRepo.findOne({
+      where: { userId: userId },
     });
     if (existUser) {
-      return res.status(400).json({ message: '중복된 아이디와 이름입니다.' });
+      return res.status(400).json({ message: '중복된 아이디 입니다.' });
     }
 
-    const user = myDataBase.getRepository(User).create({
+    const user = this.userRepo.create({
       userId,
       password: await generatePassword(password),
       userName,
+      phoneNumber,
+      userEmail
     });
-    const newUser = await myDataBase.getRepository(User).save(user);
+    const newUser =await this.userRepo.save(user);
     const accessToken = generateAccessToken(newUser.id, newUser.userId, newUser.userName);
     const refreshToken = generateRefreshToken(newUser.id, newUser.userId, newUser.userName);
     
@@ -54,10 +59,9 @@ export class UserController {
       res.status(500).json({ message: '회원 가입 중 오류가 발생하였습니다. ' });
     }
   };
-  
   static login = async (req: Request, res: Response) => {
     const { userId, password } = req.body;
-    const user = await myDataBase.getRepository(User).findOne({
+    const user = await this.userRepo.findOne({
       where: { userId },
       select: {
         id: true,
@@ -65,19 +69,15 @@ export class UserController {
         password: true
       }
     });
-
     if (!user) {
       return res.status(400).json({ message: '회원가입이 되어있지 않습니다.' });
     }
-
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(400).json({ message: '아이디 혹은 비밀번호가 일치하지 않습니다.' });
     }
-
     const accessToken = generateAccessToken(user.id, user.userId, user.userName);
     const refreshToken = generateRefreshToken(user.id, user.userId, user.userName);
-
     const decoded = verify(accessToken, process.env.SECRET_ATOKEN);
     try {
       res.cookie('refreshToken', refreshToken, { path: '/', httpOnly: true, maxAge: 3600 * 24 * 30 * 1000 });
@@ -86,7 +86,6 @@ export class UserController {
       res.status(500).json({ message: '로그인 실패하였습니다.' });
     }
   };
-
   static logout = async (req: Request, res: Response) => {
     res.clearCookie('refreshToken', { path: '/' });
     try {
@@ -95,8 +94,4 @@ export class UserController {
       res.status(500).json({ message: '로그 아웃을 실패하였습니다.' });
     }
   };
-
-
-
-  
 }
